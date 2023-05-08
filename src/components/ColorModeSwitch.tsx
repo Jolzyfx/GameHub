a3f054a6b4dcfbae77f286049edc855e32d8afa5
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-08 16:47:07 +0100

    chang

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,4 +1,4 @@
-import { HStack, LightMode, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
