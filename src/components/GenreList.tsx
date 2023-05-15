ec67ab6d6a26b0ab78270819437abd6995ecb157
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-15 11:50:35 +0100

    Refactor- simplify game query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,35 +1,55 @@
-import { Button, HStack, Heading, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
- export interface Props {
-  onSelectGenre : (genre: Genre) => void
-  selectedGenre: Genre | null
+export interface Props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenreId?: number
 }
 
-const GenreList = ({selectedGenre,onSelectGenre}: Props) => {
-  const { data , isLoading, error } = useGenres();
+const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
+  const { data, isLoading, error } = useGenres();
 
-  if (isLoading) return <Spinner/>
+  if (isLoading) return <Spinner />;
 
-  if  (error) return null
+  if (error) return null;
   return (
     <>
-    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
-    <List>
-      {data?.results.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              objectFit='cover'
-              src={getCroppedImageUrl(genre.image_background)} />
-            <Button whiteSpace={'normal'} textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold'  : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant="link">{genre.name}</Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data?.results.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                whiteSpace={"normal"}
+                textAlign="left"
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -5,7 +5,7 @@ import APIClient, { FetchResponse } from '../services/api-client.ts'
 
 
 const apiClient = new APIClient<Genre>('/genres')
-
+  
  export interface Genre {
   id: number
   name: string
@@ -16,7 +16,7 @@ const apiClient = new APIClient<Genre>('/genres')
 
 const useGenres = () => useQuery({
     queryKey: ['genres'],
-    queryFn: apiClient.getAll,
+    queryFn:  apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
     initialData: { count: genres.length, results: genres }
   })
